aecb3080675ac2ebc32570a10830e6811e733e0b
Dhananjay Chhabra <dhananjay.chhabra@patientbond.com>
2023-01-15 22:00:06 +0530

    Initial Commit

---
+++ Change App Language/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Change App Language
+//
+//  Created by Dhananjay on 15/01/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
